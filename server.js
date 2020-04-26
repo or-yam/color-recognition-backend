7fa82b797f4c4787cc8de1344afbb76f88ded343
Oryam <48219965+or-yam@users.noreply.github.com>
2020-04-26 14:32:39 +0300

    --

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const app = express();
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const entries = require('./controllers/entries');
 const userid = require('./controllers/userid');
-app.use(express.json());
-app.use(cors());
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -16,31 +15,32 @@ const db = knex({
     ssl: true,
   },
 });
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+app.use(express.json());
+app.use(cors());
 
 db.select('*')
   .from('users')
   .then((data) => {});
 
-// Check server is working
+const app = express();
+
 app.get('/', (req, res) => {
   res.send('this is working');
 });
-
 app.post('/signin', (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
 });
-
 app.post('/register', (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
-
 app.put('/image', (req, res) => {
   entries.handleEntries(req, res, db);
 });
 app.post('/imageurl', (req, res) => {
   entries.handleApiCall(req, res);
 });
-
 app.get('/profile/:id', (req, res) => {
   userid.handleUserId(req, res, db);
 });
